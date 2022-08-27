6c55a83ad27281747558be8d1f960c79b4afb6b7
Harsh Naik <harsh07may@gmail.com>
2022-08-27 10:32:31 +0530

    gitignore

--- Snackvote/src/App.jsx
+++ Snackvote/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (      
-    <Login />
-  )
+  const [count, setCount] = useState(0);
+  function getData(data){
+    console.log("Getting data:"+data);
+  }
+function getData(data)
+{
+  console.log("This is from the APp"+data);
 }
 
+  return (     
+    <Login onSubmit={ getData }/>
+  )
+}
 export default App
--- Snackvote/src/components/Login.jsx
+++ Snackvote/src/components/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import { Card,Image, Input, Button  } from 'antd';
+import img from "../../public/images/logo-1.png"
+import 'antd/dist/antd.css';
+import '../components/Login.css'
+import Password from 'antd/lib/input/Password';
 
-const Login = () => {
+const Login = (props) => {
+    const [Name, setName] = useState("");
+    const [Password, setPassword] = useState("");
+
+    function handleChange(event){
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        props.onSubmit(Name);
+    }
+
     return (
-        <div>
-            <p>Hello</p>
-        </div>
+        <>
+        <Card className='card-area' size=' large'>
+        <div className='image'><img src={img} /></div>
+            <p style={{textAlign:'center', fontSize:'32px'}}>Login</p>
+                <Input onChange={handleChange} className='input-area' placeholder="Username" size='large' />
+                <Input className='input-area' placeholder="Password" size='large' />
+                <p>{Name}</p>
+
+                <div className='button-area'><Button onClick={handleSubmit} className='btn' type="primary" size='large' >Submit</Button></div>
+        </Card>
+        </>
     );
 }
 
