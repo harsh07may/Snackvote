bbe30eeb39b86412fd7778be667bed5aafddb9d2
Harsh Naik <harsh07may@gmail.com>
2022-08-27 09:49:22 +0530

    Multiple Changes

--- Snackvote/src/App.jsx
+++ Snackvote/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      
-    </>
+  return (      
+    <Login />
   )
 }
 
